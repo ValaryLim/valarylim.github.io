9ce51783e7195c40bad8f609737cc0491dc387ca
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-07-31 16:49:39 +0800

    Add title text to site

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $(document).ready(function(){
         play: 10000,
         pagination: false
     })
+
+    var typed = new Typed('.typed', {
+        strings: ["Business Analyst", "Student"],
+        typeSpeed: 100,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    })
 }); 
