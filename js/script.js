ba63990bd6146323c6399112d278e8550e37d93d
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-07-31 15:37:34 +0800

    Add images for superslides

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // Execute when document is ready
+    $('#slides').superslides({
+        animation: 'fade',
+        // Change image every 10 seconds
+        play: 10000,
+        pagination: false
+    })
+}); 
