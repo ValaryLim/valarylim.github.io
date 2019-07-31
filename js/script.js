61b14818add6bd83fe8c2647b4085971e369e2d4
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-07-31 20:16:53 +0800

    Add carousel skeleton

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         // Change image every 10 seconds
         play: 10000,
         pagination: false
-    })
+    });
 
     var typed = new Typed('.typed', {
         strings: ["Business Analyst", "Student"],
@@ -13,5 +13,22 @@ $(document).ready(function(){
         loop: true,
         startDelay: 1000,
         showCursor: false
-    })
-}); 
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
