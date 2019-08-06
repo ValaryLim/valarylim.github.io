7484134378235bca4d169e779fd8305aa82e5d20
Valary Lim <valarylim@Valarys-MacBook-Pro.local>
2019-08-06 16:13:56 +0800

    Complete pie charts and styling

--- js/script.js
+++ js/script.js
@@ -17,18 +17,39 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        items: 4,
         responsive:{
             0:{
                 items:1
             },
-            600:{
+            480:{
+                items:2
+            },
+            768:{
                 items:3
             },
-            1000:{
-                items:5
+            938:{
+                items:4
             }
+        }
+    });
+
+    var skillsTopOffset = $('.skillsSection').offset().top;
+    $(window).scroll(function() {
+        // Animate as soon as charts get into view
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#35477D',
+                trackColor: '#fff',
+                lineWidth: 15,
+                size: 152,
+                scaleColor: false,
+                lineCap: 'round',
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
         }
     });
 });
