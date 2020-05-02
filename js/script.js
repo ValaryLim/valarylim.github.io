57171d21c7ff093364f5fcf5a117a4433630bf99
Valary Lim Wan Qian <43085808+ValaryLim@users.noreply.github.com>
2020-05-02 17:44:24 +0800

    Version 2.0 (#23)
    * change fonts and set colour scheme
    
    * update aboutme section
    
    * remove unnecessary images
    
    * update contact section
    
    * remove skillbar section
    
    * section titles and taglines
    
    * flexing options
    
    * resume - education & styling
    
    * update resume and portfolio section

--- js/script.js
+++ js/script.js
@@ -24,39 +24,53 @@ $(document).ready(function(){
             body.removeClass('fixedNav');
         }
     };
-
-    var skillsTopOffset = $(".skillsSection").offset().top;
-
-    $(window).scroll(function() {
-        if (window.pageYOffset > (skillsTopOffset - $(window).height() + 200)) {
-            $('.skillbar').skillBars({
-                // number start
-                from: 0,       
-              
-                // number end 
-                to: false,      
-              
-                // animation speed
-                speed: 800,   
-              
-                // how often the element should be up<a href="https://www.jqueryscript.net/time-clock/">date</a>d
-                interval: 100,    
-              
-                // the number of decimal places to show
-                decimals: 0,      
-              
-                // callback method for every time the element is updated,
-                onUpdate: null,   
-              
-                // callback method for when the element finishes updating
-                onComplete: null,   
-              
-                // CSS classes
-                classes:{
-                  skillBarBar : '.skillbar-bar',
-                  skillBarPercent : '.skill-bar-percent',
-                }
-            });
-        }
-    });
 });
+
+filterSelection("all");
+
+function filterSelection(c) {
+    var x = document.getElementsByClassName("filterItem");
+    if (c == "all") c = "";
+    
+    for (var i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
+  
+// Show filtered elements
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+  
+// Hide elements that are not selected
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+  
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("filter");
+var buttons = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = "btn";
+        this.className = "btn active";
+    });
+}
